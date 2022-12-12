c518d4ab9084dee743639b3321bc50527a82ac3e
Tushar7567 <swain.tushar00@gmail.com>
2022-12-12 21:37:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const data1 = require("./Routes/bollywood")
 const cors = require("cors")
 
+const mongoose = require('mongoose');
+require('dotenv').config({path:"./config.env"})
+
+
 require('./DB/connection')
 const User = require('./model/userSchema')
 
@@ -19,6 +23,23 @@ app.use("/home", data1)
 console.log(data1);
 
 
+
+
+
+const connectDB = async () => {
+    try {
+       mongoose.connect(Db);
+      console.log(`MongoDB Connected`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+}
+
+
+
+
+
 // app.use("/api/users", registerRoutes);
 app.use("/", router);
 
@@ -28,6 +49,39 @@ app.use("/", router);
 // })
 
 app.use(cookieParser());
+
+
+
+
+const Db = process.env.URI
+
+// mongoose.connect(Db).then(() => {
+//     console.log('Database connection successful');
+// }).catch((err) => {
+//     console.log('Database failed to connect');
+// })
+
+
+
+connectDB().then(() => {
+    app.listen(process.env.PORT||8080, () => {
+        console.log("server running");
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // app.listen(5000,()=>{
 //     console.log("server running");
